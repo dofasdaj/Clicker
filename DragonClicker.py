c5830a705f32408e89b798cacaa20a4bc5291085
David Roos <david@roosens.de>
2015-10-31 17:59:56 +0100

    Update

--- DragonClicker.py
+++ DragonClicker.py
@@ -36,16 +36,17 @@ UpPerC11 = False
 UpPerC12 = False
 UpPerC13 = False
 RUp1 = False
-font = pygame.font.SysFont(None, 25)
-
+#font = pygame.font.SysFont(None, 25)
 class mainLoop:
     def __init__(self):
         Taste = "Q"
         Taste2 = "E"
+        Taste3 = "A"
         DisplayGr = (1200, 700)
         ###################
         KosUp1 = 100
         KosUp2 = 200
+        KosUp3 = 1000
         Update = 1
         ###################
         PerSec1 = False
@@ -64,28 +65,51 @@ class mainLoop:
         UpPerC12 = False
         UpPerC13 = False
         RUp1 = False
+        MaUp1 = False
+        MaUp2 = False
         ####################
         punkte = 0
+        LS = 1
+        MS = 0
+        Bog = 0
         i = 0
         while Dragon_Clicker:
+            sys_font = pygame.font.SysFont(None, 35)
+            render = sys_font.render("Stärke deiner Armee:"+ str(Update),0,(0,0,0))
+            setDisplay.blit(render, (30,200))
+            sys_font = pygame.font.SysFont(None, 30)
+            render = sys_font.render("Dein Schwert:"+str(LS),0,(0,0,0))
+            setDisplay.blit(render, (30,250))
+            sys_font = pygame.font.SysFont(None, 30)
+            render = sys_font.render("Bogenschützen:"+str(Bog),0,(0,0,0))
+            setDisplay.blit(render, (30,300))
+            sys_font = pygame.font.SysFont(None, 30)
+            render = sys_font.render("Magier:"+str(MS),0,(0,0,0))
+            setDisplay.blit(render, (30,350))
             sys_font = pygame.font.SysFont(None, 60)
-            render = sys_font.render("Updatestufe:"+str(Update),0,(0,0,0))
+            render = sys_font.render("Nicht Cookie",0,(0,0,0))
             setDisplay.blit(render, (500,10))
             sys_font = pygame.font.SysFont(None, 30)
-            render = sys_font.render("Kosten fuers Klick-Update:"+" "+str(KosUp1),0,(0,0,0))
+            render = sys_font.render("Dein Schwert:"+" "+str(KosUp1),0,(0,0,0))
             setDisplay.blit(render, (850,40))
             sys_font = pygame.font.SysFont(None, 30)
-            render = sys_font.render("Druecke"+" "+str(Taste)+" "+"zum verbessern",0,(0,0,0))
+            render = sys_font.render(str(Taste)+" "+"zum verbessern.",0,(0,0,0))
             setDisplay.blit(render, (850,80))
             sys_font = pygame.font.SysFont(None, 30)
-            render = sys_font.render("Kosten fuers Sec-Update:"+" "+str(KosUp2),0,(0,0,0))
+            render = sys_font.render("Magier:"+" "+str(KosUp3),0,(0,0,0))
+            setDisplay.blit(render, (850,210))
+            sys_font = pygame.font.SysFont(None, 30)
+            render = sys_font.render(str(Taste3)+" "+"zum verbessern.",0,(0,0,0))
+            setDisplay.blit(render, (850,250))
+            sys_font = pygame.font.SysFont(None, 30)
+            render = sys_font.render("Bogenschützen:"+" "+str(KosUp2),0,(0,0,0))
             setDisplay.blit(render, (850,130))
             sys_font = pygame.font.SysFont(None, 30)
-            render = sys_font.render("Dreucke"+" "+str(Taste2)+" "+"zum verbessern",0,(0,0,0))
+            render = sys_font.render(str(Taste2)+" "+"zum verbessern",0,(0,0,0))
             setDisplay.blit(render, (850,170))
-            sys_font = pygame.font.SysFont(None, 30)
-            render = sys_font.render(("Punkte:"+str(punkte)),0,(0,0,0))
-            setDisplay.blit(render, (30,215))
+            sys_font = pygame.font.SysFont(None, 50)
+            render = sys_font.render(("Geld:"+str(punkte)),0,(0,0,0))
+            setDisplay.blit(render, (30,100))
             pygame.display.update()
             bild = pygame.image.load("CookieBild.bmp")
             bild = bild.convert()
@@ -94,6 +118,10 @@ class mainLoop:
                 punkte += 0.02
             if PerSec2 == True:
                 punkte += 0.04
+            if MaUp1 == True:
+                punkte += 0.1
+            if MaUp2 == True:
+                punkte += 0.2
             #################################
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -101,16 +129,14 @@ class mainLoop:
                     sys.exit
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if UpPerC2 == True:
-                        punkte += 1
+                        punkte += 2
                     if UpPerC3 == True:
                         punkte += 3
                     if UpPerC4 == True:
                         punkte += 4
-                    #if RUp1 == True:
-                        #i = random.randrange(10)
-                        #time.wait(10)
-                        #if i == 9:
-                            #punkte * 2
+                    if UpPerC5 == True:
+                        punkte += 5
+                    
                     else:
                         punkte += 1
                     
@@ -124,14 +150,17 @@ class mainLoop:
                             Taste2 = "R"
                             time.wait(10)
                             PerSec1 = True
+                            Bog += 1
                     if event.key == K_r:
-                        if punkte >= 500 and PerSec2 == False:
+                        if punkte >= 500 and PerSec2 == False and PerSec1 == True:
+                            PerSec2 = True
                             punkte -= 500
                             Update += 1
                             KosUp2 = 1000
                             Taste2 = "E"
                             time.wait(10)
-                            PerSec2 = True
+                            Bog += 1
+                            
                     if event.key == K_q:
                         if punkte >= 100 and UpPerC2 == False :
                             UpPerC2 = True
@@ -140,6 +169,7 @@ class mainLoop:
                             KosUp1 = 250
                             Taste = "W"
                             time.wait(10)
+                            LS += 1
                     if event.key == K_w and UpPerC2 == True:
                         if punkte >= 250 and UpPerC3 == False:
                             UpPerC3 = True
@@ -148,6 +178,7 @@ class mainLoop:
                             KosUp1 = 500
                             time.wait(10)
                             Taste = "Q"
+                            LS += 1
                     if event.key == K_q and UpPerC3 == True:
                         if punkte >= 500 and UpPerC4 == False:
                             UpPerC4 = True
@@ -156,22 +187,37 @@ class mainLoop:
                             KosUp1 = 1000
                             time.wait(10)
                             Taste = "W"
+                            LS += 1
                     if event.key == K_w and UpPerC5 == False:
                         if punkte >= 1000 and UpPerC5 == False:
-                            UpPerC3 = True
+                            UpPerC5 = True
                             punkte -= 1000
                             Update += 1
                             KosUp1 = 1000
                             time.wait(10)
                             Taste = "Q"
-                    if event.key == K_a:
-                        if punkte >= 300:
-                            punkte -= 300
+                            LS += 1
+                    if event.key == K_a and MaUp1 == False:
+                        if punkte >= 1000:
+                            MaUp1 = True
+                            punkte -= 1000
                             Update += 1
+                            KosUp3 = 3000
                             time.wait(10)
-                            RUp1 = True
-                        else:
-                             print("error")
+                            Taste3 = "S"
+                            MS += 1
+                    if event.key == K_s:
+                        if punkte >= 3000 and MaUp1 == True and MaUp2 == False:
+                            MaUp2 = True
+                            punkte -= 3000
+                            Update += 1
+                            KosUp3 = 10000
+                            time.wait(10)
+                            Taste3 = "A"
+                            MS += 1
+
+                    else:
+                        print("error")
                          
 
                     if event.key == K_m:
