7025056ce21217d00d0a490e6ca61ec626ae96aa
David Roos <david@roosens.de>
2015-11-01 11:24:22 +0100

    Update

--- DragonClicker.py
+++ DragonClicker.py
@@ -82,14 +82,14 @@ class mainLoop:
             sys_font = pygame.font.SysFont(None, 50)
             render = sys_font.render(("Geld:"+str(punkte)),0,(0,0,0))
             setDisplay.blit(render, (30,100))
-            render = sys_font.render("Stärke deiner Armee:"+ str(Update),0,(0,0,0))
+            render = sys_font.render("Staerke deiner Armee:"+ str(Update),0,(0,0,0))
             setDisplay.blit(render, (30,200))
             render = sys_font.render("Deine Wirtschaft:",0,(0,0,0))
             setDisplay.blit(render, (30,470))
             sys_font = pygame.font.SysFont(None, 30)
             render = sys_font.render("Dein Schwert:"+str(LS),0,(0,0,0))
             setDisplay.blit(render, (30,250))
-            render = sys_font.render("Bogenschützen:"+str(Bog),0,(0,0,0))
+            render = sys_font.render("Bogenschuetzen:"+str(Bog),0,(0,0,0))
             setDisplay.blit(render, (30,300))
             render = sys_font.render("Magier:"+str(MS),0,(0,0,0))
             setDisplay.blit(render, (30,350))
@@ -97,19 +97,19 @@ class mainLoop:
             setDisplay.blit(render, (30,400))
             render = sys_font.render("Dein Schwert:"+" "+str(KosUp1),0,(0,0,0))
             setDisplay.blit(render, (850,40))
-            render = sys_font.render(str(Taste)+" "+"zum verbessern.",0,(0,0,0))
+            render = sys_font.render("Q"+" "+"zum verbessern.",0,(0,0,0))
             setDisplay.blit(render, (850,80))
             render = sys_font.render("Magier:"+" "+str(KosUp3),0,(0,0,0))
             setDisplay.blit(render, (850,210))
-            render = sys_font.render(str(Taste3)+" "+"zum verbessern.",0,(0,0,0))
+            render = sys_font.render("E"+" "+"zum verbessern.",0,(0,0,0))
             setDisplay.blit(render, (850,250))
-            render = sys_font.render("Bogenschützen:"+" "+str(KosUp2),0,(0,0,0))
+            render = sys_font.render("Bogenschuetzen:"+" "+str(KosUp2),0,(0,0,0))
             setDisplay.blit(render, (850,130))
-            render = sys_font.render(str(Taste2)+" "+"zum verbessern",0,(0,0,0))
+            render = sys_font.render("W"+" "+"zum verbessern",0,(0,0,0))
             setDisplay.blit(render, (850,170))
             render = sys_font.render("Berserker:"+" "+str(KosUp4),0,(0,0,0))
             setDisplay.blit(render, (850,290))
-            render = sys_font.render(str(Taste4)+" "+"zum verbessern",0,(0,0,0))
+            render = sys_font.render("R"+" "+"zum verbessern",0,(0,0,0))
             setDisplay.blit(render, (850,330))
             
             pygame.display.update()
@@ -128,7 +128,7 @@ class mainLoop:
                     punkte += 1 * LS                    
                 ###################################
                 if event.type == KEYDOWN:
-                    #Bogenschützen
+                    #Bogenschuetzen
                     if event.key == K_w:
                         if punkte >= 200:
                             punkte -= 200
@@ -136,7 +136,7 @@ class mainLoop:
                             KosUp2 = 500
                             time.wait(10)
                             Bog += 1
-                            #Schwert
+                    #Schwert
                     if event.key == K_q:
                         if punkte >= 100:
                             punkte -= 100
@@ -160,11 +160,8 @@ class mainLoop:
                             KosUp4 = 20000
                             time.wait(10)
                             BS += 1
-
                     else:
                         print("error")
-                         
-
                     if event.key == K_m:
                         punkte += 500
                         print("Cheater ")
