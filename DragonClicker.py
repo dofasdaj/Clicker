3deb9a79e372a2a4b22d6e07216ed23081f6da1b
David Roos <david@roosens.de>
2015-11-01 15:22:19 +0100

    Es lebt immer besser

--- DragonClicker.py
+++ DragonClicker.py
@@ -21,21 +21,8 @@ setDisplay = pygame.display.set_mode((1300, 800))
 pygame.display.set_caption("Dragon Clicker")
 Dragon_Clicker = True
 i = 0
-display_X = 800
-display_Y = 400
-UpPerC2 = False
-UpPerC3 = False
-UpPerC4 = False
-UpPerC5 = False
-UpPerC6 = False
-UpPerC7 = False
-UpPerC8 = False
-UpPerC9 = False
-UpPerC10 = False
-UpPerC11 = False
-UpPerC12 = False
-UpPerC13 = False
-RUp1 = False
+display_X = 1200
+display_Y = 700
 #font = pygame.font.SysFont(None, 25)
 class mainLoop:
     def __init__(self):
@@ -64,54 +51,51 @@ class mainLoop:
         i = 0
         MiSt += SiMi
         while Dragon_Clicker:
-            sys_font = pygame.font.SysFont(None, 60)
-            render = sys_font.render("Nicht Cookie",0,(0,0,0))
-            setDisplay.blit(render, (500,10))
-            sys_font = pygame.font.SysFont(None, 35)
+            
             sys_font = pygame.font.SysFont(None, 50)
             render = sys_font.render(("Geld:"+str(round(punkte))),0,(0,0,0))
-            setDisplay.blit(render, (30,100))
+            setDisplay.blit(render, (500,10))
             render = sys_font.render("Staerke deiner Armee:"+ str(Update),0,(0,0,0))
-            setDisplay.blit(render, (30,200))
+            setDisplay.blit(render, (30,50))
             render = sys_font.render("Deine Wirtschaft:",0,(0,0,0))
-            setDisplay.blit(render, (30,470))
+            setDisplay.blit(render, (30,300))
             sys_font = pygame.font.SysFont(None, 30)
             render = sys_font.render("Dein Schwert:"+str(LS),0,(0,0,0))
-            setDisplay.blit(render, (30,250))
+            setDisplay.blit(render, (30,100))
             render = sys_font.render("Bogenschuetzen:"+str(Bog),0,(0,0,0))
-            setDisplay.blit(render, (30,300))
+            setDisplay.blit(render, (30,150))
             render = sys_font.render("Magier:"+str(MS),0,(0,0,0))
-            setDisplay.blit(render, (30,350))
+            setDisplay.blit(render, (30,200))
             render = sys_font.render("Berserker:"+str(BS),0,(0,0,0))
-            setDisplay.blit(render, (30,400))
+            setDisplay.blit(render, (30,250))
             render = sys_font.render("Minen:"+str(SiMi),0,(0,0,0))
-            setDisplay.blit(render, (30,520))
+            setDisplay.blit(render, (30,350))
             render = sys_font.render("Waffen Schmied:"+str(AnWaS),0,(0,0,0))
-            setDisplay.blit(render, (30,570))
-            render = sys_font.render("Dein Schwert:"+" "+str(KosUp1),0,(0,0,0))
-            setDisplay.blit(render, (850,40))
-            render = sys_font.render("Q"+" "+"zum verbessern.",0,(0,0,0))
-            setDisplay.blit(render, (850,80))
-            render = sys_font.render("Magier:"+" "+str(KosUp3),0,(0,0,0))
+            setDisplay.blit(render, (30,400))
+            render = sys_font.render("Dein Schwert:"+" "+str(KosUp1),0,(255,0,0))
+            setDisplay.blit(render, (850,30))
+            render = sys_font.render("Q"+" "+"zum verbessern.",0,(255,0,0))
+            setDisplay.blit(render, (850,60))
+            render = sys_font.render("Magier:"+" "+str(KosUp3),0,(255,0,0))
+            setDisplay.blit(render, (850,150))
+            render = sys_font.render("E"+" "+"zum verbessern.",0,(255,0,0))
+            setDisplay.blit(render, (850,180))
+            render = sys_font.render("Bogenschuetzen:"+" "+str(KosUp2),0,(255,0,0))
+            setDisplay.blit(render, (850,90))
+            render = sys_font.render("W"+" "+"zum verbessern",0,(255,0,0))
+            setDisplay.blit(render, (850,120))
+            render = sys_font.render("Berserker:"+" "+str(KosUp4),0,(255,0,0))
             setDisplay.blit(render, (850,210))
-            render = sys_font.render("E"+" "+"zum verbessern.",0,(0,0,0))
-            setDisplay.blit(render, (850,250))
-            render = sys_font.render("Bogenschuetzen:"+" "+str(KosUp2),0,(0,0,0))
-            setDisplay.blit(render, (850,130))
-            render = sys_font.render("W"+" "+"zum verbessern",0,(0,0,0))
-            setDisplay.blit(render, (850,170))
-            render = sys_font.render("Berserker:"+" "+str(KosUp4),0,(0,0,0))
-            setDisplay.blit(render, (850,290))
-            render = sys_font.render("R"+" "+"zum verbessern",0,(0,0,0))
+            render = sys_font.render("R"+" "+"zum verbessern",0,(255,0,0))
+            setDisplay.blit(render, (850,240))
+            render = sys_font.render("Mine:"+" "+str(KosUp5),0,(255,255,0))
+            setDisplay.blit(render, (850,270))
+            render = sys_font.render("A" +" "+"zum bauen",0,(255,255,0))
+            setDisplay.blit(render, (850,300))
+            render = sys_font.render("Waffen Schmied:"+" "+str(KosUp6),0,(0,255,0))
             setDisplay.blit(render, (850,330))
-            render = sys_font.render("Mine:"+" "+str(KosUp5),0,(0,0,0))
-            setDisplay.blit(render, (850,380))
-            render = sys_font.render("A" +" "+"zum bauen",0,(0,0,0))
-            setDisplay.blit(render, (850,420))
-            render = sys_font.render("Waffen Schmied:"+" "+str(KosUp6),0,(0,0,0))
-            setDisplay.blit(render, (850,470))
-            render = sys_font.render("S" +" "+"zum bauen",0,(0,0,0))
-            setDisplay.blit(render, (850,510))
+            render = sys_font.render("S" +" "+"zum bauen",0,(0,255,0))
+            setDisplay.blit(render, (850,360))
             
             pygame.display.update()
             bild = pygame.image.load("CookieBild.bmp")
@@ -136,7 +120,6 @@ class mainLoop:
                         if punkte >= 200:
                             punkte -= 200
                             Update += 1
-                            KosUp2 = 500
                             time.wait(10)
                             Bog += 1
                     #Schwert
@@ -151,7 +134,6 @@ class mainLoop:
                         if punkte >= 1000:
                             punkte -= 1000
                             Update += 1
-                            KosUp3 = 3000
                             time.wait(10)
                             Taste3 = "S"
                             MS += 1
@@ -176,8 +158,6 @@ class mainLoop:
                             time.wait(10)
                             WaS += 1
                             AnWaS += 1
-                    if event.key == K_m:
-                        punkte += 20000
                     #nur feur Entwickler zum Testen
 ###############################################################                        
 mainLoop()
